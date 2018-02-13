e1452a19519fff4db4d740bfe602bc840da98876
Alec German <alec_german@yahoo.com>
2018-02-13 00:26:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   def self.genre_count
     genre_hash = {}
 
-    @@genres.each do |genre|
+    @@genres.each {|genre| genre_hash[genre] = @@genres.count(genre)}
       genre_hash[genre] = @@genres.count(genre)
     end
 
